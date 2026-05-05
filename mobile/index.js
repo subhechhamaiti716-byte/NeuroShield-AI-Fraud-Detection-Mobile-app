60afaf5137a6872ebe8dc6a18c42fc4da6e30a4f
Subhechha Maiti <subhechhamaiti716@gmail.com>
2026-05-05 16:45:45 +0530

    Fix: Switch to Webpack for web stability

--- mobile/index.js
+++ mobile/index.js
@@ -1,12 +1,4 @@
-import '@expo/metro-runtime';
-import { AppRegistry } from 'react-native';
+import { registerRootComponent } from 'expo';
 import App from './App';
 
-// Manually register and run the app for web
-AppRegistry.registerComponent('main', () => App);
-if (typeof window !== 'undefined') {
-  AppRegistry.runApplication('main', {
-    initialProps: {},
-    rootTag: document.getElementById('root') || document.getElementById('main'),
-  });
-}
+registerRootComponent(App);
